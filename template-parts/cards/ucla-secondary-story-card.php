3f7b4c30c9376c0641a76cf3b0839e48aaee1bb1
Scott Gruber <scott@scottgruber.me>
2023-09-29 14:32:45 -0700

    Add story card and secondary story cards

--- template-parts/cards/ucla-secondary-story-card.php
+++ template-parts/cards/ucla-secondary-story-card.php
@@ -8,7 +8,7 @@ else
   $link_attrs = "href='".get_permalink()."'";
   }
 ?>
-<article class="ucla-card ucla-card__story h-entry post-<?php the_ID(); ?>" id="post-<?php the_ID(); ?>">
+<article class="ucla-card ucla-card__story-secondary h-entry post-<?php the_ID(); ?>" id="post-<?php the_ID(); ?>">
   <?php // Entry Meta ?>
   <figure>
   <a <?php echo $link_attrs; ?> rel="bookmark" class="story-card-image-link">
@@ -21,7 +21,7 @@ else
 <time class="ucla-card__date dt-published"
     datetime="<?php echo get_post_time( 'Y-n-j' ); ?>"><?php echo get_post_time( 'F j, Y' ); ?></time>
   <h3 class="ucla-card__title p-name"><a aria-label="<?php the_title(); ?>" class="ucla-card__title-link u-url" <?php echo $link_attrs; ?>
-      rel="bookmark"><?php the_title(); ?></a></h3>
+      rel="bookmark"><?php the_title(); ?></a></h2>
   <p class="ucla-card__description p-summary"><?= wp_strip_all_tags( get_the_excerpt(), true ) ?></p>
   
 </div>
