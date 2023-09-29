02f93bdcb0f7b84facfa6bf1103499fd4b7d1aac
Scott Gruber <scott@scottgruber.me>
2023-09-29 11:01:33 -0700

    Add css and src files to child theme

--- functions.php
+++ functions.php
@@ -1,6 +1,6 @@
 <?php
 // Load Theme Scripts and Styles
-add_action("wp_enqueue_scripts", "ucla_wordpress_child_load_scripts");
+add_action("wp_enqueue_scripts", "ucla_wordpress_child_load_scripts", 99);
 function ucla_wordpress_child_load_scripts() {
-	wp_enqueue_style( 'ucla-wordpress-child', get_template_directory_uri() . "/css/ucla-ps.min.css", [], null, "screen" );
+	wp_enqueue_style( 'ucla-wordpress-child', get_stylesheet_directory_uri() . "/css/ucla-ps.min.css", [], null, "screen" );
 }
